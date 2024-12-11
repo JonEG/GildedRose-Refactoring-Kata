a7f99366eb900b7cfd722cc7c2cece1932d34d6c
Jon Echeveste González <j.echeveste-gonzales@ovan.de>
2024-12-11 13:59:45 +0100

    Initial requirements tests

--- dart/README.md
+++ dart/README.md
@@ -1,5 +1,6 @@
+# My notes
 
-# Cool fact
+## Cool fact
 
 I managed to get my dependencies while on the airplane by using
 
@@ -12,3 +13,14 @@ flutter pub get --offline
                              not. In offline mode, it will need to have all
                              dependencies already available in the pub cache to
                              succeed.
+
+## Initial requirements
+
+- Once the sell by date has passed, `Quality` degrades twice as fast
+- The `Quality` of an item is never negative
+- __"Aged Brie"__ actually increases in `Quality` the older it gets
+- The `Quality` of an item is never more than `50`
+- __"Sulfuras"__, being a legendary item, never has to be sold or decreases in `Quality`
+- __"Backstage passes"__, like aged brie, increases in `Quality` as its `SellIn` value approaches;
+  - `Quality` increases by `2` when there are `10` days or less and by `3` when there are `5` days or less but
+  - `Quality` drops to `0` after the concert
--- dart/test/gilded_rose_test.dart
+++ dart/test/gilded_rose_test.dart
@@ -1,13 +1,147 @@
+import 'package:gilded_rose/item.dart';
 import 'package:test/test.dart';
 import 'package:gilded_rose/gilded_rose.dart';
 
 main() {
-  test('foo', () {
+  test("When Item updates then quality decreases", () {
+    //Arrange
+    var item = new Item("foo", 0, 1);
+    int initialQuality = item.quality;
+    var items = <Item>[item];
+    GildedRose app = new GildedRose(items);
+    //Act
+    app.updateQuality();
+    //Assert
+    expect(initialQuality > app.items[0].quality, true);
+  });
+  test("When Item updates then days to sell decreases", () {
+    //Arrange
+    var item = new Item("foo", 0, 0);
+    int initialDays = item.sellIn;
+    var items = <Item>[item];
+    GildedRose app = new GildedRose(items);
+    //Act
+    app.updateQuality();
+    //Assert
+    expect(initialDays > app.items[0].sellIn, true);
+  });
+  test("When Item date is passed then its quality decreases twice as fast", () {
+    //Arrange
+    var item = new Item("foo", 1, 10);
+    int initialQuality = item.quality;
+    var items = <Item>[item];
+    GildedRose app = new GildedRose(items);
+    //Act
+    app.updateQuality();
+    int firstQualityUpdate = app.items[0].quality;
+    int firstQualityDecrease = initialQuality - firstQualityUpdate;
+    app.updateQuality();
+    int secondQualityUpdate = app.items[0].quality;
+    int secondQualityDecrease = firstQualityUpdate - secondQualityUpdate;
+    //Assert
+    expect(firstQualityDecrease * 2 == secondQualityDecrease, true);
+  });
+  test('When Item updates its quality is never negative', () {
+    //Arrange
     var item = new Item('foo', 0, 0);
     var items = <Item>[item];
-
     GildedRose app = new GildedRose(items);
+    //Act
     app.updateQuality();
-    expect("fixme", app.items[0].name);
+    //Assert
+    expect(app.items[0].quality >= 0, true);
+  });
+  test("When updated Item is Aged Brie then quality increases", () {
+    //Arrange
+    var item = new Item("Aged Brie", 0, 0);
+    int initialQuality = item.quality;
+    var items = <Item>[item];
+    GildedRose app = new GildedRose(items);
+    //Act
+    app.updateQuality();
+    //Assert
+    expect(initialQuality < app.items[0].quality, true);
+  });
+  test("When updated Item is Backstage passes then quality increases", () {
+    //Arrange
+    var item = new Item("Backstage passes to a TAFKAL80ETC concert", 1, 0);
+    int initialQuality = item.quality;
+    var items = <Item>[item];
+    GildedRose app = new GildedRose(items);
+    //Act
+    app.updateQuality();
+    //Assert
+    expect(initialQuality < app.items[0].quality, true);
+  });
+  test(
+      "When updated Item is Backstage and 10 days or less till concert then quality increases 2",
+      () {
+    const hotDaysLeft = [10, 6];
+    for (var daysForTheConcert in hotDaysLeft) {
+      //Arrange
+      var item = new Item(
+        "Backstage passes to a TAFKAL80ETC concert",
+        daysForTheConcert,
+        1,
+      );
+      var items = <Item>[item];
+      GildedRose app = new GildedRose(items);
+      //Act
+      app.updateQuality();
+      //assert
+      expect(app.items[0].quality, 1 + 2);
+    }
+  });
+  test(
+      "When updated Item is Backstage and 5 days or less till concert then quality increases 3",
+      () {
+    const hotDaysLeft = [5, 1];
+    for (var daysForTheConcert in hotDaysLeft) {
+      //Arrange
+      var item = new Item(
+        "Backstage passes to a TAFKAL80ETC concert",
+        daysForTheConcert,
+        1,
+      );
+      var items = <Item>[item];
+      GildedRose app = new GildedRose(items);
+      //Act
+      app.updateQuality();
+      //assert
+      expect(app.items[0].quality, 1 + 3);
+    }
+  });
+  test(
+      "When updated Item is Backstage passes and is concert day then quality falls to 0",
+      () {
+    //Arrange
+    var item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10);
+    var items = <Item>[item];
+    GildedRose app = new GildedRose(items);
+    //Act
+    app.updateQuality();
+    //Assert
+    expect(app.items[0].quality, 0);
+  });
+  test("When Item increases quality its maximum is 50", () {
+    //Arrange
+    var item = new Item("Aged Brie", 0, 50);
+    var items = <Item>[item];
+    GildedRose app = new GildedRose(items);
+    //Act
+    app.updateQuality();
+    //Assert
+    expect(app.items[0].quality, 50);
+  });
+  test("When Sulfuras updates it does not vary", () {
+    //Arrange
+    var item = new Item("Sulfuras, Hand of Ragnaros", 1, 1);
+    var items = <Item>[item];
+    GildedRose app = new GildedRose(items);
+    //Act
+    app.updateQuality();
+    //Assert
+    expect(app.items[0].quality, 1);
+    expect(app.items[0].sellIn, 1);
   });
 }
