43b555d1b0b1414e004ae460d04c2482e078268c
Jon Echeveste González <j.echeveste-gonzales@ovan.de>
2024-12-12 18:23:20 +0000

    created ItemInterface

--- dart/lib/basic_item.dart
+++ dart/lib/basic_item.dart
@@ -1,6 +1,7 @@
 import 'package:gilded_rose/item.dart';
+import 'package:gilded_rose/item_interface.dart';
 
-class BasicItem extends Item {
+class BasicItem extends Item implements ItemInterface {
   BasicItem(String name, int sellIn, int quality)
       : super(name, sellIn, quality);
 
